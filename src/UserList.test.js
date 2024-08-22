b3b985ff7700b8ade2a45c0d5a79e71c2f7e8c51
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-08-22 23:46:34 +0100

    [TEST] render the name and email of users

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,7 +2,7 @@ import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import UserList from "./UserList";
 
-test('should display users', () => {
+const renderComponent = () => {
     const users = [
         {
             name: "John",
@@ -15,7 +15,31 @@ test('should display users', () => {
     ];
     render(<UserList users={users} />);
 
+    return {
+        users
+    }
+}
+
+test('should display users', () => {
+    const { users } = renderComponent();
+
     const rows = within(screen.getByTestId("usersTable")).getAllByRole("row");
 
     expect(rows).toHaveLength(users.length);
+});
+
+test('should render the name and email of each user', () => {
+    const { users } = renderComponent();
+
+    for (let user of users) {
+        const name = screen.getByRole("cell", {
+            name: user.name
+        });
+        const email = screen.getByRole("cell", {
+            name: user.name
+        });
+
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
 });
